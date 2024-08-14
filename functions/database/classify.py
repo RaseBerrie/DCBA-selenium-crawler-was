9439bf8469618c1411c287f12596a17bf96b7a00
RaseBerrie <yjin2006@gmail.com>
2024-08-14 17:43:08 +0900

    0814 edited classifier

--- functions/database/classify.py
+++ functions/database/classify.py
@@ -13,27 +13,18 @@ def git_data_fining():
     with utils.database_connect() as conn:
         with conn.cursor() as cur:
             query = '''
-                    SELECT rd.root_url, lc.company, sr.title, sr.content, sr.id
-                    FROM conn_comp_root ccr
-                    JOIN conn_root_sub crs ON ccr.root_id = crs.root_id
-                    JOIN conn_sub_res csr ON crs.sub_id = csr.sub_id
-                    JOIN search_result sr ON csr.res_id = sr.id
-                    JOIN list_company lc ON ccr.comp_id = lc.id
-                    JOIN list_rootdomain rd ON ccr.root_id = rd.id
-                    WHERE tags = 'is_github'
-                    UNION
-                    SELECT rd.root_url, lc.company, sr.title, sr.content, sr.id
-                    FROM conn_comp_root ccr
-                    JOIN conn_root_res crr ON ccr.root_id = crr.root_id
-                    JOIN search_result sr ON crr.res_id = sr.id
-                    JOIN list_company lc ON ccr.comp_id = lc.id
-                    JOIN list_rootdomain rd ON ccr.root_id = rd.id
-                    WHERE tags = 'is_github';
-                    '''
+            SELECT label, con.id, con.res_title, con.res_content
+            FROM res_data_content con
+            JOIN res_closure clo ON clo.descendant = con.id
+            JOIN res_data_label lab ON ancestor = lab.id
+            WHERE tags = 'git' AND label IN
+            (SELECT url FROM list_rootdomain
+            UNION SELECT company FROM list_company);
+            '''
             cur.execute(query)
             datas = cur.fetchall()
     
-    keys = []
+    keys = set()
     for data in datas:
         key = ""
         key += data[0].split(".")[0] + "|"
@@ -42,119 +33,117 @@ def git_data_fining():
         target = data[2] + data[3]
         l = re.search(key, target)
         if l is not None:
-            keys.append(str(data[4]))
+            keys.add(str(data[4]))
 
     with utils.database_connect() as conn:
         with conn.cursor() as cur:
-            query = 'DELETE FROM search_result WHERE tags="is_github" AND id NOT IN ({0})'.format(",".join(keys))
+            query = 'DELETE FROM res_data_content WHERE tags="git" AND id NOT IN ({0})'.format(",".join(list(keys)))
             cur.execute(query)
+
         conn.commit()
-    return 0
+        return 0
 
 def data_fining_seq_one():
     with utils.database_connect() as conn:
         with conn.cursor() as cur:
-            # 분류: filter, filter 태그가 달렸으면 필터링 당하는 대상
+            # 분류: 퍼블릭 (일반적으로 공개할 수 있는 내용)
             query = r'''
-                    UPDATE      search_result
-                    SET         tags = 'filter'
-                    WHERE       (url REGEXP "news|product|about|list|manual|media|magazine"
-                    OR          url NOT REGEXP "\\\\")
-                    AND         url REGEXP '(\/|=)[0-9a-z-]+(\.(html))*\/*$'
-                    AND         tags NOT LIKE 'is_github'
-                    AND         tags LIKE '';
-                    '''
+            UPDATE  res_data_content
+            SET     tags = 'public'
+            WHERE   tags LIKE ''
+            AND     (res_url REGEXP 'news|product|about|list|manual|media|magazine|guide'
+            OR      res_url REGEXP '(\/|=)[0-9a-z-]+(\.(html))*\/*$')
+            AND     res_url NOT REGEXP "\\\\";
+            '''
             cur.execute(query)
 
             # 분류: 파일
             query = r'''
-                    INSERT INTO list_file (id, url)
-                    SELECT      sr.id, sr.url
-                    FROM        search_result sr
-                    LEFT JOIN   list_file fl
-                    ON          sr.id = fl.id
-                    WHERE       sr.url REGEXP "\.(pdf|xlsx|docx|pptx|hwp|txt)+$"
-                    AND         sr.tags = ''
-                    AND         fl.id IS NULL;                    
-                    '''
+            INSERT INTO res_tags_file (url)
+            SELECT      res_url
+            FROM        res_data_content
+            WHERE       res_url REGEXP "\\.(pdf|xlsx|docx|pptx|hwp|txt|ai)+$"
+            AND         tags = ''
+            '''
             cur.execute(query)
 
             # 파일 태그 업데이트
             query = r'''
-                    UPDATE      search_result sr
-                    JOIN        list_file fl
-                    ON          sr.id = fl.id
-                    SET         sr.tags = 'is_file'
-                    WHERE       sr.id = fl.id;
-                    '''
+            UPDATE  res_data_content con
+            JOIN    res_tags_file t_file
+            ON      t_file.url = con.res_url
+            SET     con.tags = 'file'
+            WHERE   t_file.url = con.res_url;
+            '''
             cur.execute(query)
+        
         conn.commit()
-    return 0
+        return 0
 
 def data_fining_seq_two():
     with utils.database_connect() as conn:
         with conn.cursor() as cur:
             # 분류: 불필요한 정보 노출
             query = r'''
-                    UPDATE      search_result
-                    SET         tags = 'is_neednot'
-                    WHERE       tags = ''
-                    AND         (title REGEXP '시스템.메.지|Apache'
-                    OR          url REGEXP 'editor|plugin/|namo|dext|CVS|root|[Rr]epository|changelog|jsessionid' 
-                    OR          content REGEXP '시스템.메.지|워드프레스');
-                    '''
+            UPDATE  res_data_content
+            SET     tags = 'expose'
+            WHERE   tags = ''
+            AND     (res_title REGEXP '시스템.메.지|Apache'
+            OR      res_url REGEXP 'editor|plugin/|namo|dext|CVS|root|[Rr]epository|changelog|jsessionid'
+            OR      res_content REGEXP '시스템.메.지|워드프레스');
+            '''
             cur.execute(query)
 
             # 분류: 관리자 페이지
             query = r'''
-                    UPDATE      search_result
-                    SET         tags = 'is_admin'
-                    WHERE       tags = ''
-                    AND         (title REGEXP '관리자|admin'
-                    OR          url REGEXP 'admin\/*$'
-                    OR          content REGEXP '관리자');
-                    '''
+            UPDATE      res_data_content
+            SET         tags = 'admin'
+            WHERE       tags = ''
+            AND         (res_title REGEXP '관리자|admin'
+            OR          res_url REGEXP 'admin\/*$'
+            OR          res_content REGEXP '관리자');
+            '''
             cur.execute(query)
 
             # 분류: 로그인 페이지
             query = r'''
-                    UPDATE      search_result
-                    SET         tags = 'is_login'
-                    WHERE       tags = ''
-                    AND         (title REGEXP '로그인|login' 
-                    OR          url REGEXP 'login\.[a-zA-Z]*$'
-                    OR          content REGEXP 'login')
-                    AND         url NOT REGEXP 'regist|password';
-                    '''
+            UPDATE  res_data_content
+            SET     tags = 'is_login'
+            WHERE   tags = ''
+            AND     (res_title REGEXP '로그인|login' 
+            OR      res_url REGEXP 'login\.[a-zA-Z]*$'
+            OR      res_content REGEXP 'login')
+            AND     res_url NOT REGEXP 'regist|password';
+            '''
             cur.execute(query)
+
         conn.commit()
-    return 0
+        return 0
 
 def update_filetype():
     with utils.database_connect() as conn:
         with conn.cursor() as cur:
             filetypes = ["pdf", "xlsx", "docx", "pptx"]
             for filetype in filetypes:
-                query = f'''
-                        UPDATE  list_file
-                        SET     filetype = '{0}'
-                        WHERE   url REGEXP '{0}+$';
-                        '''.format(filetype)
+                query = '''
+                UPDATE  res_tags_file
+                SET     filetype = '{0}'
+                WHERE   url REGEXP '{0}+$';
+                '''.format(filetype)
                 cur.execute(query)
 
             query = r'''
-                    UPDATE      list_file
-                    SET         filetype = 'others'
-                    WHERE       filetype = '';
-                    '''
+            UPDATE      res_tags_file
+            SET         filetype = 'others'
+            WHERE       filetype = '';
+            '''
             cur.execute(query)
+        
         conn.commit()
-    return 0
+        return 0
 
 def run():
-    
     data_fining_seq_one()
     update_filetype()
     data_fining_seq_two()
-
     return 0
